2c538b47ac280604b1177283162d783887953a31
dulcebucio <dulcenahomibucio@gmail.com>
2025-05-07 22:11:40 -0600

    front succesfully connected with jetson's websocket server !!!

--- src/Components/Constants.jsx
+++ src/Components/Constants.jsx
@@ -1,1 +1,3 @@
-export const flask_address = "http://192.168.5.1:8080"
+export const flask_address = "http://192.168.5.1:8080" // video and tasks
+export const web_socket_address = "ws://192.168.5.1:55000"
+
--- src/Pages/Pilot/Home/Home.jsx
+++ src/Pages/Pilot/Home/Home.jsx
@@ -2,7 +2,129 @@ import NavBar from "../../../Components/Pilot/Navbar/Navbar";
 import Camera from "../../../Components/Pilot/Camera/Camera";
 import BottomNavBar from "../../../Components/Pilot/BottomNavBar/BottomNavBar"
 import "./Home.css"
+import { web_socket_address } from "../../../Components/Constants";
+import { useEffect, useState, useRef } from "react";
+
+const RANGE=1000, NEUTRAL = 0
+const THROTTLE_RANGE=500, NEUTRAL_THROTTLE = 500
+
+function scale(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
 const  Home = () => {
+  const [websocket, setWebsocket] = useState(null);
+  const [counter, setCounter] = useState(0)
+  const [powerLimit, setPowerLimit] = useState(1.0);
+  const powerLimitRef = useRef();
+  powerLimitRef.current = powerLimit;
+
+  let modes = 'MANUAL';
+
+  const calculatePotency = (joystick) => {
+    return parseInt(joystick * RANGE * powerLimitRef.current);
+  }
+  
+  const calculateThrottlePotency = (input) => {
+    const result = input * THROTTLE_RANGE * powerLimitRef.current + NEUTRAL_THROTTLE;
+    return Math.min(1000, Math.max(0, parseInt(result)));
+  }
+  
+  
+  let yawInput = 0;
+  
+
+  // websocket init
+  useEffect(() => {
+    if(counter===0){
+        const wsClient = new WebSocket(web_socket_address);
+
+        wsClient.onopen = () => {
+            setWebsocket(wsClient);
+            const start_commands_instance = {
+                throttle: 500,
+                roll: 0,
+                pitch: 0,
+                yaw: 0,
+                arm_disarm: true,
+                mode: 'MANUAL'
+            }
+            wsClient.send(JSON.stringify(start_commands_instance));
+            setWebsocket(wsClient);
+        };
+    }
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+
+      const gamepads = navigator.getGamepads()
+
+      const controller = gamepads[0]
+
+
+      let commands_yaw = 0;
+      let commands_pitch = 0;
+      let commands_roll = 0;
+      let commands_throttle = 500;
+      let commands_mode = 'MANUAL';
+
+      if(controller) {
+        const safeZone = 0.012;
+
+        // lx for roll, ly for pitch
+        const lx = controller.axes[0]
+        const ly = controller.axes[1]
+
+        // triggers for throttle
+        const lt = controller.buttons[6].value;  
+        const rt = controller.buttons[7].value;  
+
+        commands_pitch = ( ly > safeZone || ly < -safeZone) ? calculatePotency(-ly) : NEUTRAL;  
+        commands_roll = (lx > safeZone || lx < -safeZone) ? calculatePotency(lx) : NEUTRAL;
+
+
+        const throttleInput = rt - lt; // RT increases, LT decreases
+        commands_throttle = Math.abs(throttleInput) > safeZone
+          ? calculateThrottlePotency(throttleInput)
+          : NEUTRAL_THROTTLE;
+        
+        let yawInput = 0;
+
+        // Simple digital bumpers
+        if (controller.buttons[4].pressed) {
+          yawInput -= 1;
+        }
+        if (controller.buttons[5].pressed) {
+          yawInput += 1;
+        }
+
+        commands_yaw = yawInput !== 0 ? calculatePotency(yawInput) : NEUTRAL;
+
+
+        // letters for modes
+        if (controller.buttons[3].pressed) commands_mode = 'MANUAL';
+        else if (controller.buttons[1].pressed) commands_mode = 'STABILIZE';
+        else if (controller.buttons[0].pressed) commands_mode = 'ACRO';
+      
+      }
+      if(websocket !== null){
+        websocket.onmessage = (event) => {
+            const commands_instance = {
+                throttle: commands_throttle,
+                roll: commands_roll,
+                pitch: commands_pitch,
+                yaw: commands_yaw,
+                arm_disarm: true,
+                mode: commands_mode,
+            }
+            websocket.send(JSON.stringify(commands_instance))
+        }
+    }
+    }, 4)
+  })
+
+
     return (
         <div className="Background-home">
             <NavBar/>
