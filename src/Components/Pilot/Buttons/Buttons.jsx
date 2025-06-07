d264688d002dea0f535ddab3e245cd0eaf3d0eee
AntonioMarban <393tony393@gmail.com>
2025-06-07 15:26:44 -0600

    brightness, alpha, beta and start measuring features on "controls" navbar added

--- src/Components/Pilot/Buttons/Buttons.jsx
+++ src/Components/Pilot/Buttons/Buttons.jsx
@@ -1,51 +1,153 @@
-import React, { useState } from 'react';
-import Icon from '../../../assets/Icon.svg'
-import TecXotic from '../../../assets/TecXotic.svg'
-import './buttons.css'
-const Buttons =({ option, setOption, getSliderValue, onChange }) => { 
-    const [speed, setSpeed] = useState(100);
-    return (
+import React, { useState } from "react";
+import Icon from "../../../assets/Icon.svg";
+import TecXotic from "../../../assets/TecXotic.svg";
+import STARTMEASURING from '../../../assets/startMeasuring.svg'
+import "./buttons.css";
+const Buttons = ({ option, setOption, getSliderValue, onChange }) => {
+  const [speed, setSpeed] = useState(100);
+  return (
     <>
-    <div className='navbar-background'> 
+      <div className="navbar-background">
         <div>
-            <button 
-                className={option === 0 ? 'button-selected' : 'button-normal'}
-                onClick={() => setOption(0)}>SETTINGS</button>
-            <button 
-                className={option === 1 ? 'button-selected' : 'button-normal'}
-                onClick={() => setOption(1)}>CONTROLS</button>
+          <button
+            className={option === 0 ? "button-selected" : "button-normal"}
+            onClick={() => setOption(0)}
+          >
+            SETTINGS
+          </button>
+          <button
+            className={option === 1 ? "button-selected" : "button-normal"}
+            onClick={() => setOption(1)}
+          >
+            CONTROLS
+          </button>
         </div>
-        <div className='icons'>
-            <img src={TecXotic}></img>
-            <img src={Icon}></img>
+        <div className="icons">
+          <img src={TecXotic}></img>
+          <img src={Icon}></img>
         </div>
-    </div> 
-    <div className='settings-background'>
-        {option === 0  &&  
-        <div className='settings-input-range'>
+      </div>
+      <div className="settings-background">
+        {option === 0 && (
+          <div className="settings-input-range">
             <p>
-            SPEED: <span style={{ display: 'inline-block', minWidth: '3ch', textAlign: 'right' }}>{speed}</span>
+              SPEED:{" "}
+              <span
+                style={{
+                  display: "inline-block",
+                  minWidth: "3ch",
+                  textAlign: "right",
+                }}
+              >
+                {speed}
+              </span>
             </p>
-            <input 
-                type='range'
+            <input
+              type="range"
+              min={0}
+              max={300}
+              value={speed}
+              onChange={(e) => {
+                const newSpeed = Number(e.target.value);
+                setSpeed(newSpeed);
+                getSliderValue?.(newSpeed);
+                onChange?.(newSpeed);
+              }}
+            />
+          </div>
+        )}
+        {option === 1 && (
+          <div className="controls-input-range-container">
+            <div className="controls-input-range">
+              <p>
+                BRIGHTNESS:{" "}
+                <span
+                  style={{
+                    display: "inline-block",
+                    minWidth: "3ch",
+                    textAlign: "right",
+                  }}
+                >
+                  {speed}
+                </span>
+              </p>
+              <input
+                type="range"
                 min={0}
                 max={300}
                 value={speed}
                 onChange={(e) => {
-                    const newSpeed = Number(e.target.value);
-                    setSpeed(newSpeed);
-                    getSliderValue?.(newSpeed);
-                    onChange?.(newSpeed);
+                  const newSpeed = Number(e.target.value);
+                  setSpeed(newSpeed);
+                  getSliderValue?.(newSpeed);
+                  onChange?.(newSpeed);
                 }}
-            />
-        </div>
-        }
-        {option === 1  &&  
-        <p>Vista controls</p>
-        }
-    </div>  
+              />
+            </div>
+            <div className="controls-input-range">
+              <p>
+                ALPHA:{" "}
+                <span
+                  style={{
+                    display: "inline-block",
+                    minWidth: "3ch",
+                    textAlign: "right",
+                  }}
+                >
+                  {speed}
+                </span>
+              </p>
+              <input
+                type="range"
+                min={0}
+                max={300}
+                value={speed}
+                onChange={(e) => {
+                  const newSpeed = Number(e.target.value);
+                  setSpeed(newSpeed);
+                  getSliderValue?.(newSpeed);
+                  onChange?.(newSpeed);
+                }}
+              />
+            </div>
+            <div className="controls-input-range">
+              <p>
+                BETHA:{" "}
+                <span
+                  style={{
+                    display: "inline-block",
+                    minWidth: "3ch",
+                    textAlign: "right",
+                  }}
+                >
+                  {speed}
+                </span>
+              </p>
+              <input
+                type="range"
+                min={0}
+                max={300}
+                value={speed}
+                onChange={(e) => {
+                  const newSpeed = Number(e.target.value);
+                  setSpeed(newSpeed);
+                  getSliderValue?.(newSpeed);
+                  onChange?.(newSpeed);
+                }}
+              />
+            </div>
+            <button className="button-text-icon">
+              <p>
+                START <br />
+                MEASURING
+              </p>
+              <img src={STARTMEASURING} />
+            </button>
+          </div>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Buttons;
