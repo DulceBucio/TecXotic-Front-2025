f34331360890c72c9890cee4026597b72218fc0e
dulcebucio <dulcenahomibucio@gmail.com>
2025-06-08 15:46:27 -0600

    take screenshot

--- src/Components/Pilot/BottomNavBar/BottomNavBar.jsx
+++ src/Components/Pilot/BottomNavBar/BottomNavBar.jsx
@@ -6,9 +6,34 @@ import PAUSE from '../../../assets/pause.svg'
 import RECORD from '../../../assets/record.svg'
 import Crosshair from '../CrossHair/CrossHair';
 import "./BottomNavBar.css"
+import { flask_address } from '../../Constants';
 const BottomNavBar =({rotation, roll, pitch, yaw}) => { 
-    const [record,setRecord] = useState(0);
 
+    const TakePhoto = async (camera) => {
+        try {
+            const response = await fetch(`${flask_address}/screenshot/${camera}`, {
+                method: "GET"
+            })
+
+            if (!response.ok) {
+                throw new Error('Failed to fetch image')
+            }
+
+            const blob = await response.blob();
+
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'screenshot.jpg';
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+
+        } catch (error) { 
+            console.error('Error taking photo...', error)
+        }
+    };
     return (
         <div className='Background-Bottom-Navbar'>
             
@@ -27,18 +52,13 @@ const BottomNavBar =({rotation, roll, pitch, yaw}) => {
             
             <p className='button-icon'>YAW: {yaw}°</p>
             
-            <button className='button-text-icon' onClick={() => setRecord(record === 0 ? 1 : 0)}>
-                <div>
-                {record === 0 ? 
-                    "Record" : 
-                    <>Stop<br/>Recording</>
-                }
-                </div>
-                <img src={record === 0 ? RECORD : PAUSE}/>
+            <button className='button-text-icon' onClick={() => TakePhoto(1)}>
+                <p>CAMERA <br/>1</p>
+                <img src={TAKEPHOTO}/>
             </button>
             
-            <button className='button-text-icon'>
-                <p>TAKE <br/>PHOTO</p>
+            <button className='button-text-icon' onClick={() => TakePhoto(2)}>
+                <p>CAMERA <br/>2</p>
                 <img src={TAKEPHOTO}/>
             </button>
         </div>
